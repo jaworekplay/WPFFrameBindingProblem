5f0128743bf92a5c23b389e1aa3696372d64c43a
jaworekplay <jaworekplay@gmail.com>
2016-11-01 21:37:17 +0000

    Initial Commit
    SO wouldn't let me post the code so here we are

---
+++ WpfApplication1/LoginPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Interaction logic for LoginPage.xaml
+    /// </summary>
+    public partial class LoginPage : UserControl
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WpfApplication1/MainVM.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApplication1.Commands;
+
+namespace WpfApplication1
+{
+    public class MainVM : INotifyPropertyChanged
+    {
+        public MainVM()
+        {
+            InitialiseComponents();
+        }
+
+        private void InitialiseComponents()
+        {
+            LoginCommand = new RelayCommand(loginCommandMethod);
+        }
+
+        private RelayCommand _loginCommand;
+
+        public RelayCommand LoginCommand
+        {
+            get { return _loginCommand; }
+            private set { _loginCommand = value; }
+        }
+
+        private void loginCommandMethod(object parameter)
+        {
+            // do something
+        }
+        private string searchKey;
+
+        public string SearchKey
+        {
+            get { return searchKey; }
+            set { searchKey = value; OnPropertyChanged("SearchKey"); }
+        }
+
+
+        #region INotify
+        public void OnPropertyChanged(string prop)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+        public event PropertyChangedEventHandler PropertyChanged; 
+        #endregion
+    }
+}
